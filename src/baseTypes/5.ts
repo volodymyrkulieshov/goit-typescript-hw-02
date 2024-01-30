fa03eeaa5255d142519f61618f7991f09fdd34a1
Volodymyr <vovkul@gmail.com>
2024-01-30 12:20:48 +0100

    add tasks 1-9 baseTypes

--- src/baseTypes/5.ts
+++ src/baseTypes/5.ts
@@ -3,5 +3,6 @@
   І так само визначте змінну, яка може приймати тільки одне з двох рядкових значень: 'enable' або 'disable' (literal type)?
 */
 
-let union;
-let literal;
+let union: string | number;
+type literal = "enable" | "disable";
+let literal: literal;
