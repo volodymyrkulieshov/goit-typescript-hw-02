b1d0a586fdf5c563b9c1cdcce6fa2e3e6784baf3
Volodymyr <vovkul@gmail.com>
2024-02-01 16:01:00 +0100

    add HW 02 TS

--- src/baseTypes/practice.ts
+++ src/baseTypes/practice.ts
@@ -1,19 +1,1 @@
-type User = {
-  id: number;
-  name: string;
-};
 
-const getUserNames = (users: User[]): string[] => {
-  return users.map((user) => user.name);
-};
-
-const users: User[] = [
-  { id: 1, name: "Alice" },
-  { id: 2, name: "Bob" },
-  { id: 3, name: "Charlie" },
-];
-
-let result = getUserNames(users);
-console.log(result); // ['Alice', 'Bob', 'Charlie']
-
-export {};
--- src/generic/4.ts
+++ src/generic/4.ts
@@ -1,15 +1,17 @@
 /*
   Використовуйте generics та інтерфейси, щоб виправити помилку в наступних класах:
 */
-
-class Component {
-  constructor (public props:T) {
-
-  }
+interface ComponentProps {
+  title: string;
 }
+class Component<T extends ComponentProps> {
+  constructor(public props: T) {}
+}
+interface PageProps extends ComponentProps {
 
-class Page extends Component {
-  pageInfo () {
+}
+class Page extends Component<PageProps> {
+  pageInfo() {
     console.log(this.props.title);
   }
 }
