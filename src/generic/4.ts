004e96aa38e54ebbf28bf8aea29a5e9a33d69b9b
Volodymyr <vovkul@gmail.com>
2024-02-01 16:33:46 +0100

    fixed

--- src/generic/4.ts
+++ src/generic/4.ts
@@ -4,13 +4,11 @@
 interface ComponentProps {
   title: string;
 }
-class Component<T extends ComponentProps> {
+class Component<T> {
   constructor(public props: T) {}
 }
-interface PageProps extends ComponentProps {
 
-}
-class Page extends Component<PageProps> {
+class Page extends Component<ComponentProps> {
   pageInfo() {
     console.log(this.props.title);
   }
